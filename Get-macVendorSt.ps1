e1d122d66267a744e3791af93cc8a9bf5e18d821
David Cross <david.cross@ipdevnet.com>
2020-05-05 08:12:02 -0400

    Initial commit

---
+++ Get-macVendorSt.ps1
@@ -0,0 +1,152 @@
+
+<#
+	.SYNOPSIS
+		Provides a lookup of the mac address or vendor by parsing a pre-compiled oui.txt file from IEEE
+	
+	.DESCRIPTION
+		Get-macVendorSt takes a mac address as a input value and returns the Vendor, or takes a vendor as an argument and returns a list of all mac address prefixes that match the vendor (any part) string.
+	
+	.PARAMETER macSearch
+		A description of the macSearch parameter.
+	
+	.PARAMETER vendorSearch
+		A description of the vendorSearch parameter.
+	
+	.PARAMETER ageCheck
+		A description of the ageCheck parameter.
+	
+	.NOTES
+		Other derivatives are out there for sniffers and other scripts
+#>
+function Get-macVendorSt
+{
+	[CmdletBinding()]
+	param
+	(
+		[Parameter(Mandatory = $false)]
+		[string[]]$macSearch,
+		[Parameter(Mandatory = $false)]
+		[string]$vendorSearch,
+		[int]$ageCheck = 7,
+		[switch]$refreshOnly
+	)
+	
+	#End Param
+	
+	
+	Process
+	{
+		#Check the OS and set $_dir to the slash or back slash notation
+		$_dir = ""
+		
+		if ($PSVersionTable.OS -and $PSVersionTable.OS -match "Linux")
+		{
+			$_dir = "~/.bt.diamondip"
+			$_outFile = "oui.txt"
+			$_path = "$_dir/$_outFile"
+			$_processed = "$_dir/processed_oui.txt"
+		}
+		else
+		{
+			$_dir = "~\Documents\.bt.diamondip"
+			$_outFile = "oui.txt"
+			$_path = "$_dir\$_outFile"
+			$_processed = "$_dir\processed_oui.txt"
+		}
+		
+		#If the users mac Address table does not exist, download and create it.
+		if (!(Test-Path -PathType Any $_path))
+		{
+			#Write-Output "[WARN] The oui.txt file was not found. Attempting to download now ..."
+			#Invoke-RestMethod -Method GET -Uri "http://standards-oui.ieee.org/oui.txt" -OutFile $_path
+			Get-OuiFileProcess -ouiFile $true
+		}
+		else
+		{
+			if ($refreshOnly.IsPresent -eq $true)
+			{
+				#Write-Output "[WARN] This file is older than $ageCheck days. Downloading new version now..."
+				#rm -Path $_path -ErrorAction SilentlyContinue
+				#rm -Path $_processed -ErrorAction SilentlyContinue
+				#Invoke-RestMethod -Method GET -Uri "http://standards-oui.ieee.org/oui.txt" -OutFile $_path
+				Get-OuiFileProcess -ouiFile $true
+				
+			}
+			else
+			{
+				#Write-Output "[INFO] Oui.txt file found. Checking Age of file ..."
+				$fileDate = (Get-Item $_path).CreationTime
+				$thisdate = Get-Date
+				if (($fileDate.AddDays($ageCheck)) -lt $thisdate)
+				{
+					#Write-Output "[WARN] This file is older than $ageCheck days. Downloading new version now..."
+					#rm -Path $_path -ErrorAction SilentlyContinue
+					#rm -Path $_processed -ErrorAction SilentlyContinue
+					#Invoke-RestMethod -Method GET -Uri "http://standards-oui.ieee.org/oui.txt" -OutFile $_path
+					Get-OuiFileProcess -ouiFile $file
+				}
+			}
+			
+		}
+		
+		
+		if (!(Test-Path -PathType Any $_processed) -or $refreshOnly.IsPresent -eq $true)
+		{
+			Get-OuiFileProcess -processFile $true
+		}
+		
+		$MacContents = Import-Csv -Path $_processed -Header "OUI", "VENDOR" -Delimiter "|"
+		
+		[string[]]$foundMacList = ""
+		
+		if ($macSearch)
+		{
+			
+			#Below is a good mac RX that checks for a full mac delimited
+			#$macRx = ($macSearch | Select-String -Pattern "^([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})$")
+			
+			
+			foreach ($mac in $macSearch)
+			{
+				if ($mac -gt '')
+				{
+					$macAddr = ($mac.ToString().ToUpper() | foreach { $_ -replace "[:-]", "" })
+					$macSearchString = $macAddr.Substring(0, 6)
+					
+					#$foundMac = (Get-Content "~/processed_oui.txt" -ErrorAction ignore | select-string -Pattern $macSearchString)   
+					$foundMac = ($MacContents | Select-Object -Property OUI, VENDOR | Where-Object OUI -match $macSearchString)
+				}
+				if ($foundMac)
+				{
+					Write-Output $foundMac
+					
+				}
+				else
+				{
+					Write-Output "MAC Not Found: $macSearch"
+				}
+				
+			}
+			
+		}
+		
+		
+		if ($vendorSearch -gt '')
+		{
+			
+			#This is a search for the vendor to return all mac addresses
+			#$foundVendor = (Get-Content "~/processed_oui.txt" -ErrorAction ignore | select-string -Pattern $vendorSearch) 
+			
+			$foundVendor = ($MacContents | Select-Object -Property OUI, VENDOR | Where-Object VENDOR -match $vendorSearch)
+			
+			if ($foundVendor)
+			{
+				$foundVendor
+			}
+			else
+			{
+				Write-Output "Vendor Not Found: $vendorSearch"
+			}
+		}
+	}
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+"#Powershell MAC OUI Lookup" 
